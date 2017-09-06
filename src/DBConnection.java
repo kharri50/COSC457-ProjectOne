c14625118ef27c295a5c40548e6f418aa85dbce8
Kyle harris <kyle@kyleharris.io>
2017-09-05 23:40:06 -0400

    Add baseline project framework along with DBConnection and main class

---
+++ src/DBConnection.java
@@ -0,0 +1,15 @@
+/**
+ * Created by kyleharris on 9/5/17.
+ */
+
+/**
+ * This class acts as an object to perform database connectivity
+ * functionality. The constructors will initiate a new database
+ * connection via the JDBC drivers included in the external libraries.
+ *
+ */
+public class DBConnection {
+
+
+}
+
---
+++ src/main.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Kyle Harris on 9/5/17.
+ */
+
+public class main {
+    public static void main (String [] args){
+        // function just tests the database connectivity
+    }
+}
