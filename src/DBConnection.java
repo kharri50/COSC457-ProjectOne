55f8fca5e022a3af02f50a3c11f5743841a00ff7
Kyle harris <kyle@kyleharris.io>
2017-09-07 12:57:11 -0400

    Add result set reader with basic column pagnation functionality

--- src/DBConnection.java
+++ src/DBConnection.java
@@ -1,7 +1,6 @@
 /**
  * Created by kyleharris on 9/5/17.
  */
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -16,28 +15,49 @@ import java.sql.Statement;
 public class DBConnection {
 
 
+    private Connection conn; // database connection instance variable
 
-    public void connectToAndQueryDatabase(String username, String password) {
 
+
+    public void connectToDatabase(String cxn_url ,String username, String password) {
         try {
-
+            // example db url : "jdbc:mysql://localhost/lahman2016";
             Class.forName ("com.mysql.jdbc.Driver").newInstance ();
-            String DB_URL = "jdbc:mysql://localhost/lahman2016";
-
-            Connection conn = DriverManager.getConnection(DB_URL, "root", "Nnormann11!");
-            Statement stmt = conn.createStatement();
-            ResultSet rs = stmt.executeQuery("SELECT * FROM Appearances");
-
-            System.out.println(rs.toString());
-            while (rs.next()) {
-                String s = rs.getString("yearID");
-                System.out.println(s);
-            }
-
-
+            String DB_URL = cxn_url;
+            this.conn = DriverManager.getConnection(DB_URL, username , password);
         } catch (Exception e) {
             // print the error
+            System.out.println(e.getMessage());
         }
     }
+
+    /** The following function returns a ResultSet object after the
+     * SQL query has been run.
+     */
+
+    private ResultSet queryResult(String query) throws Exception{
+        Statement stmt = this.conn.createStatement();
+        return stmt.executeQuery(query);
+    }
+
+    /* The two functions work in conjunction - the private function doesn't contain any error handling, mainly
+        because it doesn't need to. The public function will handle it if it arises.
+     */
+
+    public ResultSet executeQuery(String query){
+
+        ResultSet rs;
+        try{
+            rs = this.queryResult(query);
+        }catch(Exception e){
+            //
+            rs = null; // well rs isn't going to be anything if it's a bad query so init as null
+            System.out.println(e.getMessage());
+        }
+        return rs;
+    }
+
+
+
 }
 
--- src/main.java
+++ src/main.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.sql.ResultSet;
+
 /**
  * Created by Kyle Harris on 9/5/17.
  */
@@ -6,7 +9,27 @@ public class main {
     public static void main (String [] args){
         // function just tests the database connectivity
 
+
+        String url  = "jdbc:mysql://localhost/lahman2016";
+        String user =  "root";
+        String password = "Nnormann11!";
         DBConnection dbc = new DBConnection();
-        dbc.connectToAndQueryDatabase("root", "Nnormann11!");
+        dbc.connectToDatabase(url, user, password);
+
+        // the DBConnection class only handles the connection and the queries. It doesn't parse the result sets
+
+
+        String query = "SELECT * FROM teams;";
+        //Just a simple test for the ResultReader class
+        ResultSet r  = dbc.executeQuery("SELECT * FROM teams;");
+        ResultReader reader = new ResultReader(dbc.executeQuery(query));
+
+        // code snippet will iterate the arraylist
+        ArrayList<String> columns = reader.getColumns();
+        for(String col : columns){
+            System.out.println(col);
+        }
+
+
     }
 }
