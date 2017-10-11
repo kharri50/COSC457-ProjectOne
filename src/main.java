6caa67e134172a00a7e8c11d1ee851c60a8346b0
Kyle harris <kyle@kyleharris.io>
2017-10-11 17:22:08 -0400

    database connection switched aws

--- src/main.java
+++ src/main.java
@@ -10,18 +10,19 @@ public class main {
         // function just tests the database connectivity
 
 
-        String url  = "jdbc:mysql://localhost/lahman2016";
+
+        String url  = "jdbc:mysql://18.220.133.85:3306/exsurfacepro";
         String user =  "root";
-        String password = "Nnormann11!";
+        String password = "cosc457!";
         DBConnection dbc = new DBConnection();
         dbc.connectToDatabase(url, user, password);
 
         // the DBConnection class only handles the connection and the queries. It doesn't parse the result sets
 
 
-        String query = "SELECT * FROM teams;";
+        String query = "SELECT * FROM test_users";
         //Just a simple test for the ResultReader class
-        ResultSet r  = dbc.executeQuery("SELECT * FROM teams;");
+        // ResultSet r  = dbc.executeQuery("SELECT * FROM teams;");
         ResultReader reader = new ResultReader(dbc.executeQuery(query));
 
         // code snippet will iterate the arraylist
@@ -29,6 +30,7 @@ public class main {
         for(String col : columns){
             System.out.println(col);
         }
+
 
 
     }
